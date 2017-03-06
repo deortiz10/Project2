6e2ce4bbaddf31393dc0f3dbb8060bb71f598db6
Santiago Robayo <s.robayo222@uniandes.edu.co>
2017-03-06 18:24:55 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,99 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+var assert = require('assert');
+
+var url = 'mongodb://zantler:Masita007@ds119810.mlab.com:19810/pokemondb';
+
+router.get('/', function(req, res, next) {
+    res.render('index',{ title: 'Pokemon Viewer'});
+});
+
+//ver todos los pokemones
+router.get('/get-data', function(req, res, next) {
+    var resultArray = [];
+    mongo.connect(url, function(err, db) {
+        assert.equal(null, err);
+        var cursor = db.collection('pokemon').find();
+        cursor.forEach(function(doc, err) {
+            assert.equal(null, err);
+            resultArray.push(doc);
+        }, function() {
+            db.close();
+            res.render('index', { title: 'Pokemon Viewer', pokemons: resultArray});
+        });
+    });
+});
+
+router.get('/get-teams', function(req, res, next) {
+    var resultArray = [];
+    mongo.connect(url, function(err, db) {
+        assert.equal(null, err);
+        var cursor = db.collection('teams').find();
+        cursor.forEach(function(doc, err) {
+            assert.equal(null, err);
+            resultArray.push(doc);
+        }, function() {
+            db.close();
+            res.render('index', { title: 'Pokemon Viewer', teams: resultArray});
+        });
+    });
+});
+
+router.post('/insert', function(req, res, next) {
+    var item = {
+        name: req.body.nombre,
+        level: req.body.level,
+        nature: req.body.nature,
+        //abiity: req.body.ability,
+        evs: req.body.evs,
+        ivs: req.body.ivs
+    };
+
+    mongo.connect(url, function(err, db) {
+        assert.equal(null, err);
+        db.collection('teams').insertOne(item, function(err, result) {
+            assert.equal(null, err);
+            console.log('Item inserted');
+            db.close();
+        });
+    });
+
+    res.redirect('/get-data');
+});
+
+router.post('/update', function(req, res, next) {
+    var item = {
+        level: req.body.level,
+        nature: req.body.nature,
+        //abiity: req.body.ability,
+        evs: req.body.evs,
+        ivs: req.body.ivs
+    };
+    var id = req.body.id;
+
+    mongo.connect(url, function(err, db) {
+        assert.equal(null, err);
+        db.collection('teams').updateOne({"_id": objectId(id)}, {$set: item}, function(err, result) {
+            assert.equal(null, err);
+            console.log('Item updated');
+            db.close();
+        });
+    });
+});
+
+router.post('/delete', function(req, res, next) {
+    var id = req.body.id;
+
+    mongo.connect(url, function(err, db) {
+        assert.equal(null, err);
+        db.collection('teams').deleteOne({"_id": objectId(id)}, function(err, result) {
+            assert.equal(null, err);
+            console.log('Item deleted');
+            db.close();
+        });
+    });
+});
+
+module.exports = router;
